bd856ec91977a073492e7d57058c93db71c4e0ae
ojarumaru1212 <yoshiaki.ojarumaru@gmail.com>
2021-05-02 18:05:06 +0900

    keyを環境変数で持たすよう修正

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import os
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -12,8 +13,12 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('rICc2cLl8No5EvDmFzgE4KJ0a6RKYtKwiL3Uxdb4nrOUB0EoxQjvYV49/koccNKu98txJBs73tf8r2zwHcAyJdfgNs1YuRlq6lJCPT2zDOl+bd6NWOjIxh1O25hDNSwWpHQ/wNT4FJBCV68CmGsSNwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('f80a0306848c0fd11b9a44baec6839d7')
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/")
 def hello_world():
@@ -32,11 +37,9 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -44,6 +47,7 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
-
 if __name__ == "__main__":
-    app.run()
+#    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
